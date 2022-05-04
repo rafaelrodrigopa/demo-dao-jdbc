cb584593d29fbc44ed08bb431a9b6e475dd76455
rafaelrodrigopa <rafael.rodrigo.p.a@gmail.com>
2022-05-04 02:06:23 -0300

    findById Implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,11 +1,12 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 
 	// é um forma de injeção de dependencia implicita
 	public static SellerDao createSellerDao() {
-		return new SellerDaoJDBC();
+		return new SellerDaoJDBC(DB.getConnection());
 	}
 }
