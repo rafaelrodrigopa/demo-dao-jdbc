f129bde7b1a9629719ec2e2a97222bde1f5d352d
rafaelrodrigopa <rafael.rodrigo.p.a@gmail.com>
2022-05-04 21:04:31 -0300

    Update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -42,6 +42,15 @@ public class Program {
 		sellerDao.insert(newSeller);
 		
 		System.out.println("Inserted! New id = " + 	newSeller.getId());
+		
+		System.out.println("\n=== TEST 5: seller update ===");
+		
+		seller = sellerDao.findById(9);
+		seller.setName("Martha Wayne");
+		
+		sellerDao.update(seller);
+		System.out.println("Update completed");
+		
 	}
 
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -20,7 +20,6 @@ public class SellerDaoJDBC implements SellerDao {
 
 	private Connection conn;
 	
-	
 	public SellerDaoJDBC(Connection conn) {
 		this.conn = conn;
 	}
@@ -64,7 +63,7 @@ public class SellerDaoJDBC implements SellerDao {
 		} catch (SQLException e) {
 			throw new DbException(e.getMessage());
 		} finally {
-			DB.closeConnection();
+			DB.closeStatement(st);
 		}
 		
 		
@@ -72,7 +71,28 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public void update(Seller obj) {
-		// TODO Auto-generated method stub
+		
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(""
+					+ "UPDATE seller "
+					+ "SET Name = ?, Email = ?, BirthDate = ?, BaseSalary = ?, DepartmentId = ? "
+					+ "WHERE Id = ?");
+			
+			st.setString(1, obj.getName());
+			st.setString(2, obj.getEmail());
+			st.setDate(3, new java.sql.Date(obj.getBirthDate().getTime()));
+			st.setDouble(4, obj.getBaseSalary());
+			st.setInt(5, obj.getDepartment().getId());
+			st.setInt(6, obj.getId());
+			
+			st.executeUpdate();
+			
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+		}
 		
 	}
 
